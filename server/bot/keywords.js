113ac9523331d61a1e186f2bdd5ffc710e59c64b
Sam <sjstorment22@gmail.com>
2020-09-04 18:22:28 -0500

    Made it so that the database keywords don't work in the general channel

--- server/bot/keywords.js
+++ server/bot/keywords.js
@@ -1,6 +1,10 @@
 const db = require('../db/db');   // lets us use the db, don't need to call setup here because we've already called in in app.js
 
 function checkDbKeywords(msg, content) {
+
+    // if the message is to the general channel just do nothing. these messages are too spammy in general
+    if (msg.channel.id == '265396584899018752') { return; }
+
     // get all the keywords in the database and check if the message sent contains a keyword
     db.getKeywords((err, keywords) => {
         // look at each keyword
